04eed7fe54be84473e6beed6a18381d87a984ac0
RedBox <lznmslsbsz@gmail.com>
2024-12-02 01:03:25 +0800

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,6 @@
+# the disable_ble_workflow() was removed in CircuitPython 8.2.x release
+# uncomment the lines below if CircuitPython version <= 7.3
+
+# import supervisor
+
+# supervisor.disable_ble_workflow()
